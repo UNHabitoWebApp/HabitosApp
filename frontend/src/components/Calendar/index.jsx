c95deb4b704908f5dff9fed427b26ccebc39f81b
Juan <josemargri3@gmail.com>
2025-01-23 11:14:57 -0500

    feature: Implementando de manera provisional una vision de un solo dia

--- frontend/src/components/Calendar/index.jsx
+++ frontend/src/components/Calendar/index.jsx
@@ -1,4 +1,5 @@
 import {
+    useCurrentTimeAndDate,
     useDateActions,
     useDateSelectors,
 } from "../../hooks/useDateActions";
@@ -17,42 +18,88 @@ const events = [
     { name: "Dormir", description: "Descansar", start: "23:00", end: "23:59" },
 ];
 
+const monthMap = {
+    January: '01',
+    February: '02',
+    March: '03',
+    April: '04',
+    May: '05',
+    June: '06',
+    July: '07',
+    August: '08',
+    September: '09',
+    October: '10',
+    November: '11',
+    December: '12',
+};
+
 const Calendar = () => {
     const { updateCurrentTimeAction, resetDateAction } = useDateActions();
     const { daysOfWeek, remainingTime, remainingTimeForNextMinute } = useDateSelectors();
 
     useTimeUpdater(remainingTime, remainingTimeForNextMinute, updateCurrentTimeAction, resetDateAction);
 
+    const onlyDay = true;    // Cambiar según el estado deseado.
+    const { currentDate } = useCurrentTimeAndDate();
+
+    const currentDayIndex = daysOfWeek.findIndex(day =>
+        `${day.year}-${monthMap[day.month]}-${day.date}` === currentDate
+    );
+
     return (
         <div className="w-[90%] h-full bg-white flex flex-col border border-gray-200 rounded-md shadow-sm">
             {/* Contenedor de días de la semana */}
             <div className="flex flex-none pr-[0.30rem] pl-[45px]">
-                {daysOfWeek.map((day, index) => (
-                    <DayLayout
-                        day={day}
-                        index={index}
-                        key={index}
-                    />
-                ))}
+                {onlyDay ? (
+                    currentDayIndex !== -1 && (
+                        <DayLayout
+                            day={daysOfWeek[currentDayIndex]}
+                            index={6}
+                            key={currentDayIndex}
+                        />
+                    )
+                ) : (
+                    daysOfWeek.map((day, index) => (
+                        <DayLayout
+                            day={day}
+                            index={index}
+                            key={index}
+                        />
+                    ))
+                )}
             </div>
 
             {/* Contenedor de DayFragments */}
             <div className="flex flex-grow overflow-y-auto pl-[45px] flex-2">
-                {daysOfWeek.map((day, index) => (
-                    <div
-                        key={`fragment-${index}`}
-                        className="flex-grow flex flex-col items-center w-[14.28%] min-w-[14.28%]"
-                    >
-                        <DayFragment
-                            first={index === 0}
-                            last={index === daysOfWeek.length - 1}
-                            events={[events[index]]}
-                        />
-                    </div>
-                ))}
+                {onlyDay ? (
+                    currentDayIndex !== -1 && (
+                        <div
+                            key={`fragment-${currentDayIndex}`}
+                            className="flex-grow flex flex-col items-center w-full"
+                        >
+                            <DayFragment
+                                first={true}
+                                last={true}
+                                events={events.filter((_, idx) => idx === currentDayIndex)}
+                            />
+                        </div>
+                    )
+                ) : (
+                    daysOfWeek.map((day, index) => (
+                        <div
+                            key={`fragment-${index}`}
+                            className="flex-grow flex flex-col items-center w-[14.28%] min-w-[14.28%]"
+                        >
+                            <DayFragment
+                                first={index === 0}
+                                last={index === daysOfWeek.length - 1}
+                                events={[events[index]]}
+                            />
+                        </div>
+                    ))
+                )}
             </div>
         </div>
-
     );
 };
 
