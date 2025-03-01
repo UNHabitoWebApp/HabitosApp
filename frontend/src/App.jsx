0086b69462611df51b99eb072877031592931825
Camila Zapata <mzapataar@unal.edu.co>
2025-03-01 14:21:12 -0500

    feature: cambio en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,6 @@
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom';
-import Registro from './pages/Registro.jsx';
-import React from 'react';
 import AppRoutes from './routes/AppRoutes.jsx';
-
-// function App() {
-// 	return (
-// 		<div>
-// 			<Registro />
-// 		</div>
-// 	);
-// }
-
-// export default App;
-
 
 const App = () => {
   return (
